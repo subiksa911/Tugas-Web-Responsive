6acf78a11f70ed5e24c0a9fab1594ce489d63dec
Ketut Gede Subiksa <subiksaketutgede2000@gmail.com>
2021-02-26 14:16:55 +0800

    Website-responsive

---
+++ Website.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Subiksa Store</title>
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body >
+        <div class="container">
+            <header class="header">
+
+                <div class="hero">
+                    <h3 class="sub-heading" style="text-decoration-color: white;">RTX 3000 Series</h3>
+                </div>
+                <div class="feature feature-1">
+                    <h4 class="price">IDR 15.000.000</h4>    
+                    </div>
+                <div class="feature feature-2">
+                    <h4 class="price">IDR 10.000.000</h4>
+                    </div>
+            </header>
+
+            <nav class="menu">
+                <div class="brand">
+                <h1> Subiksa Official Website</h1>
+                </div>
+
+                <ul class="menu-list">
+                    <li><a href="">Beranda</a></li>
+                    <li><a href="">Produk</a></li>
+                    <li><a href="">Tentang</a></li>
+                    <li><a href="">Galeri</a></li>
+                </ul>
+            </nav>
+            <section class="services">
+                <div class="service">
+                    <div class="icon"><img src="img/Rtx2080ti.png"></div>
+                    <h3>RTX 2080 Ti</h3>
+                    <p>IDR 22.500.000</p>
+                    
+                </div>
+                <div class="service">
+                    <div class="icon"><img src="img/rtx2080super.png"></div>
+                    <h3>RTX 2080 Super</h3>
+                    <p>IDR Rp21.725.000</p>
+                    
+                </div>
+                <div class="service">
+                    <div class="icon"><img src="img/rtx2080biasa.png"></div>
+                    <h3>RTX 2080</h3>
+                    <p>IDR 15.700.000</p>
+                    
+                </div>
+            </section>
+        </div>
+    </body>
+</html>
